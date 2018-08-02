58441a0f02b898ba37c63cda1b608de086c77e1f
fengjinyu <fengjinyu@mobike.com>
2018-08-02 16:27:40 +0800

    1

---
+++ src/main/java/algorithms/leetcode/list/LC_234_Palindrome_Linked_List.java
@@ -0,0 +1,35 @@
+package algorithms.leetcode.list;
+
+public class LC_234_Palindrome_Linked_List {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
