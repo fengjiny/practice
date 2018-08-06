c13dafd85f49edfc2ed71f9d5cd46dda7f2cae1a
fengjinyu <fengjinyu@mobike.com>
2018-08-06 20:13:22 +0800

    1

---
+++ src/main/java/algorithms/leetcode/list/LC_147_Insertion_Sort_List.java
@@ -0,0 +1,24 @@
+package algorithms.leetcode.list;
+
+public class LC_147_Insertion_Sort_List {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+        ListNode newHead = new ListNode(0);
+        ListNode cur = head;
+        ListNode pre = newHead;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            while (pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = newHead;
+            cur = next;
+        }
+        return newHead.next;
+
+    }
+}
