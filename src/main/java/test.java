fa44ffdc101dfb1f121d738218f932cb5348e11a
fengjinyu <fengjinyu@mobike.com>
2018-08-08 22:50:52 +0800

    1

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        amount(25.65);
+    }
+    private static void amount(double htPrice) {
+        int ht = 10283;
+        double sum = ht * htPrice + 2366;
+        System.out.println(sum);
+    }
+}
