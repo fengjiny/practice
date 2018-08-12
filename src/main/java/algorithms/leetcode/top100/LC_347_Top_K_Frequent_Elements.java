184a1ab6dc08245dd16b0b5caf56d9da6ea3558a
fengjinyu <fengjinyu@mobike.com>
2018-08-12 13:14:20 +0800

    1

---
+++ src/main/java/algorithms/leetcode/top100/LC_347_Top_K_Frequent_Elements.java
@@ -0,0 +1,9 @@
+package algorithms.leetcode.top100;
+
+import java.util.List;
+
+public class LC_347_Top_K_Frequent_Elements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+    }
+}
