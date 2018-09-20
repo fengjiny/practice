bce2aa1087ca941955e75285a464095900ce677a
fengjinyu <fengjinyu@mobike.com>
2018-09-20 11:30:08 +0800

    1

--- src/main/java/thintkinginjava4/chapter_18/a.java
+++ src/main/java/thintkinginjava4/chapter_18/a.java
@@ -1,4 +1,9 @@
 package thintkinginjava4.chapter_18;
 
 public class a {
+    public static void main(String[] args) {
+        String a = "8.4.0";
+        String b = "8.4.0";
+        System.out.println(a.compareTo(b));
+    }
 }
