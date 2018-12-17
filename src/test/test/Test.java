350c45ede34f42c6bdd32c0aa4d4140fe0100cd7
fengjinyu <fengjinyu@mobike.com>
2018-12-17 11:54:45 +0800

    1

--- src/test/test/Test.java
+++ src/test/test/Test.java
@@ -8,18 +8,24 @@ import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) {
-//        HashMap hashMap = new HashMap();
-//
-//        for (int i = 0; i < 12; ++i) {
-//            hashMap.put(i, i + "");
-//        }
-//        hashMap.put(1,11);
-//        hashMap.put(1,12);
-//        hashMap.put(1,13);
-//        hashMap.put(1,14);
-//        System.out.println(hashMap);
-//        test2();
-        test3();
+        String cityCOde_2018_12_07  = "010\n" +
+                "021\n" +
+                "0311\n" +
+                "0531\n" +
+                "0351\n" +
+                "024\n" +
+                "0551\n" +
+                "0773\n" +
+                "025\n" +
+                "0571\n" +
+                "0471\n" +
+                "0472\n" +
+                "0539\n" +
+                "0537\n" +
+                "0538\n" +
+                "0536\n";
+        test4(cityCOde_2018_12_07);
+
     }
 
     public static void test1() {
@@ -47,60 +53,11 @@ public class Test {
         treeMap.put(null, null);
     }
 
-    private static void test3() {
-        String s = "027\n" +
-                "0378\n" +
-                "0393\n" +
-                "0715\n" +
-                "0724\n" +
-                "028\n" +
-                "0931\n" +
-                "0838\n" +
-                "1833\n" +
-                "0816\n" +
-                "0951\n" +
-                "0831\n" +
-                "0833\n" +
-                "0817\n" +
-                "0991\n" +
-                "0875\n" +
-                "0835\n" +
-                "1994\n" +
-                "020\n" +
-                "0755\n" +
-                "0592\n" +
-                "0771\n" +
-                "0754\n" +
-                "0898\n" +
-                "0756\n" +
-                "0899\n" +
-                "0595\n" +
-                "0763\n" +
-                "1754\n" +
-                "0577\n" +
-                "0551\n" +
-                "0574\n" +
-                "0510\n" +
-                "0515\n" +
-                "0519\n" +
-                "0514\n" +
-                "0517\n" +
-                "0315\n" +
-                "0531\n" +
-                "0534\n" +
-                "0312\n" +
-                "0532\n" +
-                "0411\n" +
-                "0354\n" +
-                "0536\n" +
-                "0357\n" +
-                "0472\n" +
-                "0478\n" +
-                "0543\n" +
-                "0349\n";
+    private static void test3(String citiCodeStr) {
+
         List<String> cityCodes = new ArrayList<>();
         Pattern p = Pattern.compile("(.*?)\\n");
-        Matcher m = p.matcher(s);
+        Matcher m = p.matcher(citiCodeStr);
 
         while (m.find()) {
             String res = m.group(1);
@@ -122,6 +79,24 @@ public class Test {
 
         sb.append("]");
         List list = new LinkedList();
+        System.out.println(sb.toString());
+    }
+
+    private static void test4(String cityCodeStr) {
+        String[] cityCodeArray = cityCodeStr.split("\n");
+        int lenth = cityCodeArray.length;
+        StringBuffer sb = new StringBuffer();
+        sb.append("[");
+        for (int i = 0; i < lenth; ++i) {
+            sb.append('"');
+            sb.append(cityCodeArray[i]);
+            sb.append('"');
+            if (i < lenth - 1) {
+                sb.append(",");
+            }
+            System.out.println(cityCodeArray[i]);
+        }
+        sb.append("]");
         System.out.println(sb.toString());
     }
 }
