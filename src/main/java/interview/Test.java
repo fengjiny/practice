0a259eeaf159f2ce0009ee9b6900a1305ed85887
fengjinyu <fengjinyu@mobike.com>
2019-01-21 18:24:57 +0800

    1

--- src/main/java/interview/Test.java
+++ src/main/java/interview/Test.java
@@ -1,4 +1,26 @@
 package interview;
 
 public class Test {
+
+    public int searchInRotateArray(int[] nums, int val) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == val) return mid;
+            if (nums[mid] > nums[left]) {
+                if (val > nums[left] && val < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (val > nums[mid] && val < nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
