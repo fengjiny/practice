d44935bd99ca634f6cf903666ceabf1017b2e2a7
fengjinyu <fengjinyu@mobike.com>
2019-02-12 10:41:39 +0800

    1

---
+++ src/main/java/algorithms/leetcode_20181226/array/LC_26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,14 @@
+package algorithms.leetcode_20181226.array;
+
+public class LC_26_Remove_Duplicates_from_Sorted_Array {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int i = 1;
+        for (int j = 1; j < nums.length; ++j) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
