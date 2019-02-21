cc59d4cf3ec2d65456ea620969a5a7fd9c5fca8a
fengjinyu <fengjinyu@mobike.com>
2019-02-21 15:56:59 +0800

    1

---
+++ src/main/java/algorithms/leetcode_20181226/tree/LC_230_Kth_Smallest_Element_in_a_BST.java
@@ -0,0 +1,21 @@
+package algorithms.leetcode_20181226.tree;
+
+
+public class LC_230_Kth_Smallest_Element_in_a_BST {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k - 1 - count);
+        }
+        return root.val;
+    }
+    private int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+
+
+}
