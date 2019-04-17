6aa8d6eef3e3b03ce17f6ea64ca42b521d6796fb
fengjinyu <fengjinyu@mobike.com>
2019-04-17 14:54:59 +0800

    1

--- src/main/java/algorithms/DeleteOddPosNode.java
+++ src/main/java/algorithms/DeleteOddPosNode.java
@@ -5,24 +5,29 @@ package algorithms;
 public class DeleteOddPosNode {
     public ListNode deleteOdd(ListNode head) {
         if (head == null || head.next == null) return head;
-        int count = 1;
-        ListNode cur = head;
-        while (cur.next != null && cur.next.next != null) {
-            if ((count++ & 1) == 1) {
-                cur.val = cur.next.val;
-                cur.next = cur.next.next;
-            } else {
-                cur = cur.next;
-            }
+        ListNode even = head.next, evenHead = even;
+        while (even != null && even.next != null){
+            even.next = even.next.next;
+            even = even.next;
         }
-        if (count % 1 == 1)
-            cur.next = null;
+        return evenHead;
+    }
+
+    public ListNode deleteEven(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode odd = head;
+        while (odd != null && odd.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+        }
         return head;
     }
 
     public static void main(String[] args) {
         ListNode head = Utils.createList(new int[] {1,2,3,4,5,6,7});
-        ListNode newHead = new DeleteOddPosNode().deleteOdd(head);
-        Utils.printList(newHead);
+//        ListNode newHead = new DeleteOddPosNode().deleteOdd(head);
+        ListNode newHead2 = new DeleteOddPosNode().deleteEven(head);
+//        Utils.printList(newHead);
+        Utils.printList(newHead2);
     }
 }
