38a012874faa39df934f72a8bec9102c5e3c5fe6
fengjinyu <fengjinyu@mobike.com>
2019-09-16 20:09:00 +0800

    20190916

--- src/main/java/algorithms/leetcode/leetcode_20190902/tree/LC_1008_Construct_Binary_Search_Tree_from_Preorder_Traversal.java
+++ src/main/java/algorithms/leetcode/leetcode_20190902/tree/LC_1008_Construct_Binary_Search_Tree_from_Preorder_Traversal.java
@@ -2,23 +2,26 @@ package algorithms.leetcode.leetcode_20190902.tree;
 
 import algorithms.leetcode.TreeNode;
 
+import java.util.Stack;
+
 public class LC_1008_Construct_Binary_Search_Tree_from_Preorder_Traversal {
     public TreeNode bstFromPreorder(int[] preorder) {
-        if (preorder == null) return null;
-        return build(preorder, 0, preorder.length - 1);
-    }
-    private TreeNode build(int[] nums, int start, int end) {
-        if (start > end) return null;
-        int idx = start;
-        for (int i = start + 1; i <= end; ++i) {
-            if (nums[i] > nums[idx]) {
-                idx = i - 1;
-                break;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.push(root);
+        for (int i = 1; i < preorder.length; ++i) {
+            TreeNode node = new TreeNode(preorder[i]);
+            if (preorder[i] < stack.peek().val) {
+                stack.peek().left = node;
+            } else {
+                TreeNode parent = stack.peek();
+                while (!stack.isEmpty() && preorder[i] > stack.peek().val) {
+                    parent = stack.pop();
+                }
+                parent.right = node;
             }
+            stack.push(node);
         }
-        TreeNode node = new TreeNode(nums[start]);
-        node.left = build(nums, start + 1, idx);
-        node.right = build(nums, idx + 1, end);
-        return node;
+        return root;
     }
 }
