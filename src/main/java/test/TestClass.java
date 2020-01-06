9d17002ba93d5f7d8429b036bfe1f9d75fd2ea6d
fengjinyu <fengjinyu@mobike.com>
2020-01-06 14:31:16 +0800

    1

--- src/main/java/test/TestClass.java
+++ src/main/java/test/TestClass.java
@@ -6,4 +6,18 @@ public class TestClass {
     public int inc(){
         return ++i;
     }
+
+    public static void main(String[] args) {
+        int r = 1000000;
+        for (int i = -r; i < r; ++i) {
+            for (int j = -r; j < r; ++j) {
+                if ((4*i + 4*j + i*j) == 45) {
+                    System.out.println("i: " + i);
+                    System.out.println("j: " +j);
+
+                    System.out.println(i + j);
+                }
+            }
+        }
+    }
 }
